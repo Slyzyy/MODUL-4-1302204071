4d0c96e48140a40e0534106cc527a96eca149bfa
MUHAMMAD AUZAN ZAKY ERYANA <auzanzaky63@gmail.com>
2022-03-19 12:07:24 +0700

    menambahkan class SimpleDataBase

--- Modul4_1302204071/Program.cs
+++ Modul4_1302204071/Program.cs
@@ -10,6 +10,37 @@ namespace Modul4_1302204071
     {
         static void Main(string[] args)
         {
+            SimpleDataBase<int> data = new SimpleDataBase<int>();
+            data.AddNewData(12);
+            data.AddNewData(34);
+            data.AddNewData(56);
+            data.PrintAllData();
+        }
+
+        class SimpleDataBase<T>
+        {
+            private List<T> storedData;
+            private List<DateTime> inputDates;
+
+            public SimpleDataBase()
+            {
+                storedData = new List<T>();
+                inputDates = new List<DateTime>();
+            }
+
+            public void AddNewData(T data)
+            {
+                storedData.Add(data);
+                inputDates.Add(DateTime.Now);
+            }
+
+            public void PrintAllData()
+            {
+                for (int i = 0; i < storedData.Count; i++)
+                {
+                    Console.WriteLine("Data " + i + " berisi : " + storedData[i].ToString() + ", yang disimpan pada waktu" + inputDates + i);
+                }
+            }
         }
     }
 }
